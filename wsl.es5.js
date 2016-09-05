0ea170c579a20c8e250c2425a8e92cce5789512e
John Haugeland <stonecypher@gmail.com>
2016-09-05 15:19:39 -0700

    Updates

--- wsl.es5.js
+++ wsl.es5.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
-var _constants = require('./constants.js');
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.codeNlFix = undefined;
+
+var _constants = require("./constants.js");
 
 var consts = _interopRequireWildcard(_constants);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-console.log(consts.version);
+function codeNlFix() {
+    Array.from(document.querySelectorAll("pre code")).map(function (p) {
+        return p.innerHTML = p.innerHTML.trim();
+    });
+}
+
+if (window) {
+    window.codeNlFix = codeNlFix;
+}
+
+exports.codeNlFix = codeNlFix;
